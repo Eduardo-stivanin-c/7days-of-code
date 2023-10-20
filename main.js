647e25b4394349d05788894a333962fa6f732234
Eduardo-stivanin-c <145277449+Eduardo-stivanin-c@users.noreply.github.com>
2023-10-20 08:08:35 -0300

    Atualizar o main.js

--- main.js
+++ main.js
@@ -11,13 +11,13 @@ if ( numeroUm == Number(stringUm)) {
   console.log('As variáveis numeroUm e stringUm não tem o mesmo valor')
 }
 
-if (COMPARAR O numeroTrinta e a stringTrinta) {
+if (numeroTrinta  == Number(stringTrinta)) {
   console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo')
 } else {
   console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo')
 }
 
-if (COMPARAR O numeroDez e a stringDez) {
+if (numeroDez == Number(stringDez)) {
   console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes')
 } 
 else {
