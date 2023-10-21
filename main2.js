9b495d994cf720e4a33623db49f58a39ef228081
Eduardo-stivanin-c <145277449+Eduardo-stivanin-c@users.noreply.github.com>
2023-10-21 13:40:20 -0300

    Atualizar o main2.js

--- main2.js
+++ main2.js
@@ -1,1 +1,9 @@
+/*- Qual o seu nome?
+- Quantos anos você tem?
+- Qual linguagem de programação você está estudando?*/
 
+
+
+var resposta="Olá sou Eduardo , eu tenho 21anos e já estou aprendendo Javascript !";
+
+console.log(resposta)
