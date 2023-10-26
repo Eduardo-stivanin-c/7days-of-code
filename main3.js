b213c4a5315aea5f996f4662ffed8f57408446eb
Eduardo-stivanin-c <145277449+Eduardo-stivanin-c@users.noreply.github.com>
2023-10-26 08:47:31 -0300

    atualização do 3º dia de código

--- main3.js
+++ main3.js
@@ -1,40 +1,69 @@
 
+var  cidade="";
+//const Roma_Coliseo="";
+//var  cidade_SP=String("day3/pexels-caio-45917.jpg");
 
+
+const meumenu=document.querySelectorAll(".cidade");
+
+
+
+//imagem remota pra amaenizar o problmea d desempenho do site ser lento porque imagnes locais lentifica
+
+var carregarimagens=[      String("day3/pexels-wal-lod-14615839.jpg") , String ('day3/pexels-caio-45917.jpg'),String( "day3/pexels-filippo-bergamaschi-2767739.jpg")];
+
+//,String( "day3/pexels-filippo-bergamaschi-2767739.jpg")
+//pegando os botões
+
+<<<<<<< HEAD
+=======
 
 const cidade_SP="day3/pexels-caio-45917.jpg";
 //const imagens = Roam_coliseu  , cudiade de São Pulao fot
 const imagens="day3/pexels-wal-lod-14615839.jpg",cidade_SP;
+>>>>>>> e2813d6a8e5eb7ca81306aa3d4f2cc79fe076deb
 var imagem="";
 //for (){
+  /**/
+
+/*
 if (cidade === “Roma”){
     // mostre a foto do “Coliseu”
 mostrarfoto();
-}//}
+}
+*/
+//}
+/**/
 
 
-
-mostrarfoto(imagem){
-
+function mostrarfoto(ID_imagem){
+  //se id na nas lista de imanges for nulo ou < -0 akertara de id invalido
+ /* if (Number(ID_imagem)===null || ID_imagem < 0 ){
+    alert("ID invalido pra imagem");
+  }
+  */
+ 
 //trocafumdo da div
-imagemdiv.style="background-image:url(imagem)";
+foto.src= String(carregarimagens[ID_imagem]);
+
+  
+  //mostra que foi achada a foto
+return foto.src
 }
 
-var meumenu=document.querySelectorAll(".menu");
-if(document.addEventListener){
-document.addEventListener('contextmenu',function e()
-{
-menu[0].style.display='block';
-menu[0].style.marginLeft=e.clientX + 'px';
-menu[0].style.marginTop=e.clientY + 'px';
 
-e.preventDefault();
-},false);
+
+
+for ( let contador=0;contador< meumenu.length;contador++) {
+ 
+  meumenu[contador].onclick=function(){
+    
+  mostrarfoto(contador);
+
+
+     
+};
+//acessa o console do navegador
+  console.log(contador);
 }
-else{ document.AttachEvent('oncontext',
-function(){
-  menu[0].style.display='block';
-menu[0].style.marginLeft=e.clientX + 'px';
-menu[0].style.marginTop=e.clientY + 'px';
-window.event.returnValue=false;
 
-})}; 
